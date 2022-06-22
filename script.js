cb459c885405fb1b85db1d47de839bf96c6f41e6
TheNicoleMwaura <nikkikanyeri@gmail.com>
2022-06-21 20:17:39 -0400

    changes to javascript

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 // TODO: add code here
+    window.addEventListener("load", function () {
+       
+
+        
+        fetch("https://handlers.education.launchcode.org/static/planets.json").then(function (response) {
+            return response.json()}).then(function (json) {
+
+                const div = document.getElementById("container");
+                let container = '';
+
+                for(let i=0; i<json.length; i++){
+                    container += `
+               <div class="astronaut">
+                <div class="bio">
+                   <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                    <ul>
+                     <li>Hours in space: ${json[i].hoursInSpace}</li>
+                     <li>Active: ${json[i].active}</li>
+                     <li>Skills: ${json[i].skills}</li>
+                    </ul>
+                </div>
+            <img class="avatar" src=${json[i].picture}/>
+          </div>
+                    
+                 `};
+
+              div.innerHTML = container;
+
+            });
+        
+        });
+
+    
